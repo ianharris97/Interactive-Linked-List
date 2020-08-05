6f39f9e08c378169e2f27f24e8d38e74fb007ae1
ianharris97 <37945255+ianharris97@users.noreply.github.com>
2020-08-05 15:39:45 -0500

    connect buttons to linked list and clear data in input field

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
 let list = new LinkedList();
 
+// add functions
 function addHeadButton() {
-    let data = document.getElementById('addHead').value;
-    list.addHead(data);
+    let data = document.getElementById('addHead-data');
+    list.addHead(data.value);
     list.printList();
+    data.value = '';
+}
+
+function addBeforeButton() {
+    let node = document.getElementById('addBefore-node');
+    let data = document.getElementById('addBefore-data');
+    list.addBefore(node.value, data.value);
+    list.printList();
+    node.value = '';
+    data.value = '';
+}
+
+function addAfterButton() {
+    let node = document.getElementById('addAfter-node');
+    let data = document.getElementById('addAfter-data');
+    list.addAfter(node.value, data.value);
+    list.printList();
+    node.value = '';
+    data.value = '';
+}
+
+function addTailButton() {
+    let data = document.getElementById('addTail-data');
+    list.addTail(data.value);
+    list.printList();
+    data.value = '';
+}
+
+// remove functions
+function removeHeadButton() {
+    list.removeHead();
+    list.printList();
+}
+
+function removeTailButton() {
+    list.removeTail();
+    list.printList();
+}
+
+function removeNodeButton() {
+    let node = document.getElementById('removeNode-node');
+    list.removeNode(node.value);
+    list.printList();
+    node.value = '';
 }
